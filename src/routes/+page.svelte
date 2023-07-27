625e19efd08892b3e7fa30c30d52a4a635b05d24
Jay Liu <jliu2589@gmail.com>
2023-07-27 17:52:02 -0400

    Showcase and outline

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<main>
+	<h1>Bangkok 2023</h1>
+	<div>
+		<img src="/bangkok.jpg" alt="bangkok" />
+	</div>
+	<div>
+		<h2>Day 1 - Saturday, September 2</h2>
+		<h3>Flight AC 858</h3>
+		<p>Booking Reference: 2JH4VL</p>
+		<p>Toronto YYZ<br /> 23:40<br /> Terminal 1</p>
+		<p>London LHR<br /> 11:50 (Sunday)<br /> Terminal 2</p>
+		<p>7 hours 10 minutes\n Breakfast, Meal</p>
+	</div>
+	<div>
+		<h2>Day 2 - Sunday, September 3</h2>
+		<p>hotel</p>
+	</div>
+</main>
