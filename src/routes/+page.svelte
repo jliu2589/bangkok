5ce6485024fb4201116ba4fd881d94765e6837f0
Jay Liu <jliu2589@gmail.com>
2023-07-28 06:45:08 -0400

    Tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,32 @@
 	<div>
 		<img src="/bangkok.jpg" alt="bangkok" />
 	</div>
-	<div>
-		<h2>Day 1 - Saturday, September 2</h2>
-		<h3>Flight AC 858</h3>
-		<p>Booking Reference: 2JH4VL</p>
-		<p>Toronto YYZ<br /> 23:40<br /> Terminal 1</p>
-		<p>London LHR<br /> 11:50 (Sunday)<br /> Terminal 2</p>
-		<p>7 hours 10 minutes\n Breakfast, Meal</p>
+	<div class="p-8">
+		<h2 class="text-4xl font-bold mb-4">Day 1 - Saturday, September 2</h2>
+		<div class="flex flex-col sm:flex-row">
+			<div
+				class="w-full sm:w-1/2 bg-black text-white p-4 mb-4 sm:mb-0 aspect-w-16 aspect-h-9 flex-shrink-0"
+			>
+				black
+			</div>
+			<div class="w-full sm:w-1/2 flex flex-col items-center p-4">
+				<h3 class="text-2xl font-bold">Flight AC 858</h3>
+				<p>Booking Reference: 2JH4VL</p>
+				<p>Toronto YYZ<br /> 23:40<br /> Terminal 1</p>
+				<p>London LHR<br /> 11:50 (Sunday)<br /> Terminal 2</p>
+				<p>7 hours 10 minutes\n Breakfast, Meal</p>
+			</div>
+		</div>
 	</div>
+
 	<div>
 		<h2>Day 2 - Sunday, September 3</h2>
 		<p>hotel</p>
 	</div>
 </main>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
